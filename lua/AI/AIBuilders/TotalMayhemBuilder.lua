3e871c948675bb335bfd68ccbdee096fd92774cc
Uveso <Archivar@Zwergenteam.de>
2024-11-14 21:57:04 +0100

    reformating builder (#39)

--- lua/AI/AIBuilders/TotalMayhemBuilder.lua
+++ lua/AI/AIBuilders/TotalMayhemBuilder.lua
@@ -1,32 +1,31 @@
+
 -- Add locals for BuilderConditions
 local UCBC = '/lua/editor/UnitCountBuildConditions.lua'
 local EBC = '/lua/editor/EconomyBuildConditions.lua'
 local MaxDefense = 0.15 -- 15% of all units can be defenses (categories.STRUCTURE * categories.DEFENSE)
-local TBC = '/lua/editor/threatbuildconditions.lua'
-
 -- ===================================================-======================================================== --
 -- ==                                        HEAVYASSAULT Builder                                            == --
 -- ===================================================-======================================================== --
-BuilderGroup{
-    BuilderGroupName = 'HEAVYASSAULT Builder', -- BuilderGroupName, initalized from AIBaseTemplates in "\lua\AI\AIBaseTemplates\"
+BuilderGroup {
+    BuilderGroupName = 'HEAVYASSAULT Builder',                           -- BuilderGroupName, initalized from AIBaseTemplates in "\lua\AI\AIBaseTemplates\"
     BuildersType = 'EngineerBuilder',
-    -- cheap Tech1 land bots
-    Builder{
+-- cheap Tech1 land bots
+    Builder {
         BuilderName = 'TM1 HEAVY Bot',
         PlatoonTemplate = 'EngineerBuilder',
         Priority = 16000,
         InstanceCount = 1,
-        BuilderConditions = { -- When do we want to build this ?
-        {
-            UCBC,
-            'HaveGreaterThanUnitsWithCategory',
-            { 1, categories.STRUCTURE * categories.ENERGYPRODUCTION - categories.TECH1 },
-        }, { EBC, 'GreaterThanEconTrend', { 0.4, 15.0 } }, { -- relative income -- Have we the eco to build it ? -- Do we need additional conditions to build it ?
-        EBC, 'GreaterThanEconStorageRatio', { 0.30, 0.90 } }, { -- Don't build it if... -- Ratio from 0 to 1. (1=100%)
-        UCBC, 'HaveLessThanUnitsWithCategory', {
-            3,
-            categories.MOBILE * categories.LAND * categories.TECH1 * categories.HEAVYASSAULT,
-        } }, { UCBC, 'UnitCapCheckLess', { 0.99 } } },
+        BuilderConditions = {
+            -- When do we want to build this ?
+            { UCBC, 'HaveGreaterThanUnitsWithCategory', { 1, categories.STRUCTURE * categories.ENERGYPRODUCTION - categories.TECH1 }},
+            -- Do we need additional conditions to build it ?
+            -- Have we the eco to build it ?
+            { EBC, 'GreaterThanEconTrend', { 0.4, 15.0 } }, -- relative income
+            { EBC, 'GreaterThanEconStorageRatio', { 0.30, 0.90 } },             -- Ratio from 0 to 1. (1=100%)
+            -- Don't build it if...
+            { UCBC, 'HaveLessThanUnitsWithCategory', { 3, categories.MOBILE * categories.LAND * categories.TECH1 * categories.HEAVYASSAULT }},
+            { UCBC, 'UnitCapCheckLess', { 0.99 } },
+       },
         BuilderType = 'Any',
         BuilderData = {
             Construction = {
@@ -34,28 +33,30 @@ BuilderGroup{
                 NumAssistees = 2,
                 BuildClose = false,
                 AdjacencyCategory = 'SHIELD STRUCTURE',
-                BuildStructures = { 'H1CheapBot' },
+                BuildStructures = {
+                    'H1CheapBot',
+                },
                 Location = 'LocationType',
-            },
-        },
+            }
+        }
     },
-    -- Tech1 Gunships
-    Builder{
+-- Tech1 Gunships
+    Builder {
         BuilderName = 'TM1 HEAVY Gunship 1st',
         PlatoonTemplate = 'EngineerBuilder',
         Priority = 16000,
         InstanceCount = 1,
-        BuilderConditions = { -- When do we want to build this ?
-        {
-            UCBC,
-            'HaveGreaterThanUnitsWithCategory',
-            { 3, categories.STRUCTURE * categories.LAND * categories.FACTORY },
-        }, { EBC, 'GreaterThanEconTrend', { 0.4, 15.0 } }, { -- relative income -- Have we the eco to build it ? -- Do we need additional conditions to build it ?
-        EBC, 'GreaterThanEconStorageRatio', { 0.30, 0.90 } }, { -- Don't build it if... -- Ratio from 0 to 1. (1=100%)
-        UCBC, 'HaveLessThanUnitsWithCategory', {
-            3,
-            categories.MOBILE * categories.AIR * categories.TECH1 * categories.HEAVYASSAULT,
-        } }, { UCBC, 'UnitCapCheckLess', { 0.99 } } },
+        BuilderConditions = {
+            -- When do we want to build this ?
+            { UCBC, 'HaveGreaterThanUnitsWithCategory', { 3, categories.STRUCTURE * categories.LAND * categories.FACTORY }},
+            -- Do we need additional conditions to build it ?
+            -- Have we the eco to build it ?
+            { EBC, 'GreaterThanEconTrend', { 0.4, 15.0 } }, -- relative income
+            { EBC, 'GreaterThanEconStorageRatio', { 0.30, 0.90 } },             -- Ratio from 0 to 1. (1=100%)
+            -- Don't build it if...
+            { UCBC, 'HaveLessThanUnitsWithCategory', { 3, categories.MOBILE * categories.AIR * categories.TECH1 * categories.HEAVYASSAULT }},
+            { UCBC, 'UnitCapCheckLess', { 0.99 } },
+       },
         BuilderType = 'Any',
         BuilderData = {
             Construction = {
@@ -63,32 +64,30 @@ BuilderGroup{
                 NumAssistees = 2,
                 BuildClose = false,
                 AdjacencyCategory = 'SHIELD STRUCTURE',
-                BuildStructures = { 'H1GunShip' },
+                BuildStructures = {
+                    'H1GunShip',
+                },
                 Location = 'LocationType',
-            },
-        },
+            }
+        }
     },
-    Builder{
+    Builder {
         BuilderName = 'TM1 HEAVY Gunship 2nd',
         PlatoonTemplate = 'EngineerBuilder',
         Priority = 16000,
         InstanceCount = 1,
-        BuilderConditions = { -- When do we want to build this ?
-        {
-            UCBC,
-            'HaveGreaterThanUnitsWithCategory',
-            { 3, categories.STRUCTURE * categories.ENERGYPRODUCTION - categories.TECH1 },
-        }, { -- Do we need additional conditions to build it ?
-        UCBC, 'PoolGreaterAtLocation', {
-            'LocationType',
-            1,
-            'ENGINEER TECH1',
-        } }, { EBC, 'GreaterThanEconTrend', { 2.0, 40.0 } }, { -- relative income -- Have we the eco to build it ?
-        EBC, 'GreaterThanEconStorageRatio', { 0.35, 0.90 } }, { -- Don't build it if... -- Ratio from 0 to 1. (1=100%)
-        UCBC, 'HaveLessThanUnitsWithCategory', {
-            3,
-            categories.MOBILE * categories.AIR * categories.TECH1 * categories.HEAVYASSAULT,
-        } }, { UCBC, 'UnitCapCheckLess', { 0.99 } } },
+        BuilderConditions = {
+            -- When do we want to build this ?
+            { UCBC, 'HaveGreaterThanUnitsWithCategory', { 3, categories.STRUCTURE * categories.ENERGYPRODUCTION - categories.TECH1 }},
+            -- Do we need additional conditions to build it ?
+            { UCBC, 'PoolGreaterAtLocation', { 'LocationType', 1, 'ENGINEER TECH1' }},
+            -- Have we the eco to build it ?
+            { EBC, 'GreaterThanEconTrend', { 2.0, 40.0 } }, -- relative income
+            { EBC, 'GreaterThanEconStorageRatio', { 0.35, 0.90 } },             -- Ratio from 0 to 1. (1=100%)
+            -- Don't build it if...
+            { UCBC, 'HaveLessThanUnitsWithCategory', { 3, categories.MOBILE * categories.AIR * categories.TECH1 * categories.HEAVYASSAULT }},
+            { UCBC, 'UnitCapCheckLess', { 0.99 } },
+       },
         BuilderType = 'Any',
         BuilderData = {
             Construction = {
@@ -96,78 +95,86 @@ BuilderGroup{
                 NumAssistees = 2,
                 BuildClose = false,
                 AdjacencyCategory = 'SHIELD STRUCTURE',
-                BuildStructures = { 'H1GunShip' },
+                BuildStructures = {
+                    'H1GunShip',
+                },
                 Location = 'LocationType',
-            },
-        },
+            }
+        }
     },
 }
 -- ===================================================-======================================================== --
 -- ==                                         FACTORY Builder                                                == --
 -- ===================================================-======================================================== --
-BuilderGroup{
-    BuilderGroupName = 'FACTORY Builder', -- BuilderGroupName, initalized from AIBaseTemplates in "\lua\AI\AIBaseTemplates\"
+BuilderGroup {
+    BuilderGroupName = 'FACTORY Builder',                           -- BuilderGroupName, initalized from AIBaseTemplates in "\lua\AI\AIBaseTemplates\"
     BuildersType = 'FactoryBuilder',
-    Builder{
+    Builder {
         BuilderName = 'U3 T1 HardTanks Panic',
         PlatoonTemplate = 'T3 Tech1 Mayhem Tank',
         Priority = 18700,
-        BuilderConditions = { -- When do we want to build this ?
-        { TBC, 'EnemyThreatGreaterThanValueAtBase', { 'LocationType', 5, 'Land' } },
-        -- Respect UnitCap
-        { UCBC, 'UnitCapCheckLess', { 0.97 } } },
+        BuilderConditions = {
+            -- When do we want to build this ?
+            { UCBC, 'EnemyUnitsGreaterAtLocationRadius', {  50, 'LocationType', 1, categories.MOBILE * categories.LAND - categories.SCOUT }}, -- radius, LocationType, unitCount, categoryEnemy
+            -- Do we need additional conditions to build it ?
+            -- Have we the eco to build it ?
+            -- Don't build it if...
+            -- Respect UnitCap
+            { UCBC, 'UnitCapCheckLess', { 0.97 } },
+        },
         BuilderType = 'Land',
     },
-    Builder{
+    Builder {
         BuilderName = 'U3 T1 HardTanks Spam',
         PlatoonTemplate = 'T3 Tech1 Mayhem Tank',
         Priority = 500,
-        BuilderConditions = { -- When do we want to build this ?
-        -- Do we need additional conditions to build it ?
-        -- Have we the eco to build it ?
-        { EBC, 'GreaterThanEconTrend', { 0.0, 0.0 } }, { -- relative income
-        EBC, 'GreaterThanEconStorageRatio', { 0.05, 0.90 } }, { -- Don't build it if... -- Ratio from 0 to 1. (1=100%)
-        UCBC, 'LocationFactoriesBuildingLess', {
-            'LocationType',
-            1,
-            'MOBILE TECH1 MK3',
-        } }, { UCBC, 'UnitCapCheckLess', { 0.90 } } }, -- Respect UnitCap
+        BuilderConditions = {
+            -- When do we want to build this ?
+            -- Do we need additional conditions to build it ?
+            -- Have we the eco to build it ?
+            { EBC, 'GreaterThanEconTrend', { 0.0, 0.0 } },                      -- relative income
+            { EBC, 'GreaterThanEconStorageRatio', { 0.05, 0.90 } },             -- Ratio from 0 to 1. (1=100%)
+            -- Don't build it if...
+            { UCBC, 'LocationFactoriesBuildingLess', { 'LocationType', 1, 'MOBILE TECH1 MK3' } },
+            -- Respect UnitCap
+            { UCBC, 'UnitCapCheckLess', { 0.90 } },
+        },
         BuilderType = 'Land',
     },
 }
 -- ===================================================-======================================================== --
 -- ==                                         HEAVYASSAULT Former                                            == --
 -- ===================================================-======================================================== --
-BuilderGroup{
-    BuilderGroupName = 'HEAVYASSAULT Former', -- BuilderGroupName, initalized from AIBaseTemplates in "\lua\AI\AIBaseTemplates\"
-    BuildersType = 'PlatoonFormBuilder', -- BuilderTypes are: EngineerBuilder, FactoryBuilder, PlatoonFormBuilder.
-    -- land
-    Builder{
+BuilderGroup {
+    BuilderGroupName = 'HEAVYASSAULT Former',                               -- BuilderGroupName, initalized from AIBaseTemplates in "\lua\AI\AIBaseTemplates\"
+    BuildersType = 'PlatoonFormBuilder',                                        -- BuilderTypes are: EngineerBuilder, FactoryBuilder, PlatoonFormBuilder.
+-- land
+    Builder {
         BuilderName = 'TM1 HEAVY Land Fearless',
-        PlatoonTemplate = 'TM1 HEAVYASSAULT LAND', -- Template Name. These units will be formed. See: "UvesoPlatoonTemplatesLand.lua"
-        Priority = 500, -- Priority. 1000 is normal.
-        InstanceCount = 2, -- Number of plattons that will be formed.
+        PlatoonTemplate = 'TM1 HEAVYASSAULT LAND',                               -- Template Name. These units will be formed. See: "UvesoPlatoonTemplatesLand.lua"
+        Priority = 500,                                                         -- Priority. 1000 is normal.
+        InstanceCount = 2,                                                      -- Number of plattons that will be formed.
         BuilderData = {
             NeverGuardBases = true,
             NeverGuardEngineers = true,
             UseFormation = 'AttackFormation',
-            ThreatWeights = { IgnoreStrongerTargetsRatio = 100.0 },
+            ThreatWeights = {
+                IgnoreStrongerTargetsRatio = 100.0,
+            },
         },
-        BuilderConditions = { -- When do we want to build this ?
-        {
-            UCBC,
-            'HaveGreaterThanUnitsWithCategory',
-            { 0, categories.MOBILE * categories.LAND * categories.TECH1 * categories.HEAVYASSAULT },
-        } },
-        -- Do we need additional conditions to build it ?
-        -- Have we the eco to build it ?
-        -- Don't build it if...
-        BuilderType = 'Any', -- Build with "Land" "Air" "Sea" "Gate" or "All" Factories. - "Any" forms a Platoon.
+        BuilderConditions = {
+            -- When do we want to build this ?
+            { UCBC, 'HaveGreaterThanUnitsWithCategory', { 0, categories.MOBILE * categories.LAND * categories.TECH1 * categories.HEAVYASSAULT }},
+            -- Do we need additional conditions to build it ?
+            -- Have we the eco to build it ?
+            -- Don't build it if...
+        },
+        BuilderType = 'Any',                                                    -- Build with "Land" "Air" "Sea" "Gate" or "All" Factories. - "Any" forms a Platoon.
     },
-    -- Air
-    Builder{
+-- Air
+    Builder {
         BuilderName = 'TM1 HEAVY Air Anti-Resource',
-        PlatoonTemplate = 'TM1 HEAVYASSAULT AIR', -- Template Name. These units will be formed. See: "UvesoPlatoonTemplatesLand.lua"
+        PlatoonTemplate = 'TM1 HEAVYASSAULT AIR',                               -- Template Name. These units will be formed. See: "UvesoPlatoonTemplatesLand.lua"
         Priority = 500,
         InstanceCount = 2,
         BuilderData = {
@@ -188,15 +195,14 @@ BuilderGroup{
                 'ALLUNITS',
             },
         },
-        BuilderConditions = { -- When do we want to build this ?
-        {
-            UCBC,
-            'HaveGreaterThanUnitsWithCategory',
-            { 0, categories.MOBILE * categories.AIR * categories.TECH1 * categories.HEAVYASSAULT },
-        } },
-        -- Do we need additional conditions to build it ?
-        -- Have we the eco to build it ?
-        -- Don't build it if...
-        BuilderType = 'Any', -- Build with "Land" "Air" "Sea" "Gate" or "All" Factories. - "Any" forms a Platoon.
+        BuilderConditions = {
+            -- When do we want to build this ?
+            { UCBC, 'HaveGreaterThanUnitsWithCategory', { 0, categories.MOBILE * categories.AIR * categories.TECH1 * categories.HEAVYASSAULT }},
+            -- Do we need additional conditions to build it ?
+            -- Have we the eco to build it ?
+            -- Don't build it if...
+        },
+        BuilderType = 'Any',                                                    -- Build with "Land" "Air" "Sea" "Gate" or "All" Factories. - "Any" forms a Platoon.
     },
+
 }
--- lua/AI/AIBuilders/TotalMayhemDefenseUpgrade.lua
+++ lua/AI/AIBuilders/TotalMayhemDefenseUpgrade.lua
@@ -1,58 +1,68 @@
 local EBC = '/lua/editor/EconomyBuildConditions.lua'
 local MIBC = '/lua/editor/MiscBuildConditions.lua'
 
-BuilderGroup{
+BuilderGroup {
     BuilderGroupName = 'TM PointDefensUpgrade',
     BuildersType = 'PlatoonFormBuilder',
-    Builder{
+    Builder {
         BuilderName = 'T1 PointDefens Upgrade',
         PlatoonTemplate = 'T1PointDefensUpgrade',
         Priority = 4000,
         InstanceCount = 5,
-        BuilderConditions = { -- When do we want to build this ?
-        -- Do we need additional conditions to build it ?
-        {
-            MIBC,
-            'FactionIndex',
-            { 1, 3 },
-        }, { EBC, 'GreaterThanEconTrend', { 0.4, 10.0 } }, { EBC, 'GreaterThanEconStorageRatio', { 0.30, 0.99 } } }, -- Have we the eco to build it ? -- 1: UEF, 2: Aeon, 3: Cybran, 4: Seraphim, 5: Nomads -- relative income 4 mass, 100 energy -- Ratio from 0 to 1. (1=100%)
-        -- Don't build it if...
-        -- Respect UnitCap
-        BuilderData = { NumAssistees = 2 },
-        BuilderType = 'Any',
+        BuilderConditions = {
+            -- When do we want to build this ?
+            -- Do we need additional conditions to build it ?
+            { MIBC, 'FactionIndex', { 1, 3 }}, -- 1: UEF, 2: Aeon, 3: Cybran, 4: Seraphim, 5: Nomads
+            -- Have we the eco to build it ?
+            { EBC, 'GreaterThanEconTrend', { 0.4, 10.0 } }, -- relative income 4 mass, 100 energy
+            { EBC, 'GreaterThanEconStorageRatio', { 0.30, 0.99}}, -- Ratio from 0 to 1. (1=100%)
+            -- Don't build it if...
+            -- Respect UnitCap
+        },
+        BuilderData = {
+            NumAssistees = 2,
+        },
+        BuilderType = 'Any'
     },
-    Builder{
+    Builder {
         BuilderName = 'T1 PointDefens Upgrade Experimental',
         PlatoonTemplate = 'T1PointDefensUpgradeEXP',
         Priority = 4000,
         InstanceCount = 5,
-        BuilderConditions = { -- When do we want to build this ?
-        -- Do we need additional conditions to build it ?
-        {
-            MIBC,
-            'FactionIndex',
-            { 1, 3 },
-        }, { EBC, 'GreaterThanEconTrend', { 0.8, 10.0 } }, { EBC, 'GreaterThanEconStorageRatio', { 0.40, 0.99 } } }, -- Have we the eco to build it ? -- 1: UEF, 2: Aeon, 3: Cybran, 4: Seraphim, 5: Nomads -- relative income 8 mass, 100 energy -- Ratio from 0 to 1. (1=100%)
-        -- Don't build it if...
-        -- Respect UnitCap
-        BuilderData = { NumAssistees = 2 },
-        BuilderType = 'Any',
+        BuilderConditions = {
+            -- When do we want to build this ?
+            -- Do we need additional conditions to build it ?
+            { MIBC, 'FactionIndex', { 1, 3 }}, -- 1: UEF, 2: Aeon, 3: Cybran, 4: Seraphim, 5: Nomads
+            -- Have we the eco to build it ?
+            { EBC, 'GreaterThanEconTrend', { 0.8, 10.0 } }, -- relative income 8 mass, 100 energy
+            { EBC, 'GreaterThanEconStorageRatio', { 0.40, 0.99}}, -- Ratio from 0 to 1. (1=100%)
+            -- Don't build it if...
+            -- Respect UnitCap
+        },
+        BuilderData = {
+            NumAssistees = 2,
+        },
+        BuilderType = 'Any'
     },
-    Builder{
+    Builder {
         BuilderName = 'T2 PointDefens Upgrade',
         PlatoonTemplate = 'T2PointDefensUpgrade',
         Priority = 4000,
         InstanceCount = 5,
-        BuilderConditions = { -- When do we want to build this ?
-        -- Do we need additional conditions to build it ?
-        {
-            MIBC,
-            'FactionIndex',
-            { 1, 3 },
-        }, { EBC, 'GreaterThanEconTrend', { 0.8, 10.0 } }, { EBC, 'GreaterThanEconStorageRatio', { 0.40, 0.99 } } }, -- Have we the eco to build it ? -- 1: UEF, 2: Aeon, 3: Cybran, 4: Seraphim, 5: Nomads -- relative income 8 mass, 100 energy -- Ratio from 0 to 1. (1=100%)
-        -- Don't build it if...
-        -- Respect UnitCap
-        BuilderData = { NumAssistees = 2 },
-        BuilderType = 'Any',
-    },
+        BuilderConditions = {
+            -- When do we want to build this ?
+            -- Do we need additional conditions to build it ?
+            { MIBC, 'FactionIndex', { 1, 3 }}, -- 1: UEF, 2: Aeon, 3: Cybran, 4: Seraphim, 5: Nomads
+            -- Have we the eco to build it ?
+            { EBC, 'GreaterThanEconTrend', { 0.8, 10.0 } }, -- relative income 8 mass, 100 energy
+            { EBC, 'GreaterThanEconStorageRatio', { 0.40, 0.99}}, -- Ratio from 0 to 1. (1=100%)
+            -- Don't build it if...
+            -- Respect UnitCap
+        },
+        BuilderData = {
+            NumAssistees = 2,
+        },
+        BuilderType = 'Any'
+    }
 }
+
